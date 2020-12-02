47cd0633216d1b7d9e2b152d751d6131c49839a3
MFogleman <marcfogleman@gmail.com>
2020-12-02 15:04:03 -0500

    Day1 part 1

---
+++ day01_01/Program.fs
@@ -0,0 +1,28 @@
+﻿open System
+open System.IO
+
+let getFile = File.ReadAllLines "inputs/day01_01_input.txt"
+let comps p x = p + x = 2020
+let multiply a b = a * b
+
+// find 2 numbers in nums that equal 2020
+let rec findDigitsToMatch2020 nums =
+    match nums with
+    | [p; q] -> [p; q]
+    | [] -> [] 
+    | p::xs -> match List.tryFind (comps p) xs with
+                | Some q -> [p; q] // "Found a value %d" value
+                | None -> findDigitsToMatch2020 xs
+
+
+[<EntryPoint>]
+let main argv =
+    getFile 
+    |> Array.toList
+    |> List.map int
+    |> findDigitsToMatch2020
+    |> List.reduce multiply
+    |> Console.WriteLine
+
+    0 // return an integer exit code
+
