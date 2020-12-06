e771fc14e7e70f2cf0efed2c9af34324a68fccaf
Marc Fogleman <marcfogleman@gmail.com>
2020-12-06 01:09:24 -0500

    day06 part 1

---
+++ day06/day06_01.fsx
@@ -0,0 +1,18 @@
+open System
+open System.IO
+open System.Text.RegularExpressions
+let getFile = File.ReadAllText "day06_input.txt"
+
+
+let normalizeInput (str: string) =
+  str.Split([|"\n\n"|], StringSplitOptions.None)
+
+let distinctChars (str: string) =
+  Regex.Replace(str, "\n", "")
+  |> Seq.distinct
+  |> Seq.length
+
+getFile
+  |> normalizeInput
+  |> Array.sumBy distinctChars
+  |> printfn "DEBUG %A"
