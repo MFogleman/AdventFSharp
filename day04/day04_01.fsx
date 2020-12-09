e52c5c1517dd0c868ebbb9fb2f2dd28368dc090d
Marc Fogleman <marcfogleman@gmail.com>
2020-12-09 17:26:02 -0500

    playing around with creating records

--- day04/day04_01.fsx
+++ day04/day04_01.fsx
@@ -5,30 +5,64 @@ nuget Fake.Core.Target //"
 open System
 open System.IO
 
-let getFile = File.ReadAllLines "day04_input.txt"
-let req = ["byr";"iyr";"eyr";"hgt";"hcl";"ecl";"pid"]
 
-let stringify acc curr =
-  match curr with
-  | "" -> acc + "&"
-  | _ -> acc + curr
+type Passport = {
+  Byr: string option
+  Iyr: string option
+  Eyr: string option
+  Hgt: string option
+  Hcl: string option
+  Ecl: string option
+  Pid: string option
+}
+let defaultPassport = {
+  Byr = None
+  Iyr = None
+  Eyr = None
+  Hgt = None
+  Hcl = None
+  Ecl = None
+  Pid = None
+}
 
-let isIn (passport: string) acc reqStr =
-  match passport.Contains reqStr with
-  | true -> acc + 1
-  | false -> acc
+let getFile = File.ReadAllText "day04_input.txt"
 
-// check over all requirements in passport.  If all are valid (7)
-// increment acc for 1 more valid passport
-let countValid acc passport =
-  match List.fold (isIn passport) 0 req with
-  | 7 -> acc + 1
-  | _ -> acc
+let toTups (str: string) =
+  let arr = str.Split(':')
+  (arr.[0], arr.[1])
 
+let buildPassport (passport: Passport) (k: string, v: string) =
+  match k with
+    | "byr" -> { passport with Byr = Some v}
+    | "iyr" -> { passport with Iyr = Some v}
+    | "eyr" -> { passport with Eyr = Some v}
+    | "hgt" -> { passport with Hgt = Some v}
+    | "hcl" -> { passport with Hcl = Some v}
+    | "ecl" -> { passport with Ecl = Some v}
+    | "pid" -> { passport with Pid = Some v}
+    | _ -> passport
+
+let countValid acc (passport: string) =
+  let sameLine = passport.Replace('\n', ' ').Split(' ')
+  let tups = Array.map toTups sameLine
+  let newPassport = Array.fold buildPassport defaultPassport tups
+
+  let change = match newPassport with
+                | {Byr=None } -> 0
+                | {Iyr=None } -> 0
+                | {Eyr=None } -> 0
+                | {Hgt=None } -> 0
+                | {Hcl=None } -> 0
+                | {Ecl=None } -> 0
+                | {Pid=None } -> 0
+                | _ -> 1
+  acc + change
+
+let getRawPassports (str: string) =
+  str.Split([|"\n\n"|], StringSplitOptions.None)
 
 getFile
-  |> Array.fold stringify ""
-  |> (fun str -> str.Split '&')
+  |> getRawPassports
   |> Array.fold countValid 0
   |> printfn "debug:: %A" // 219
 
