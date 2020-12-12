3783b8f57d48b1d8d7b15f09589328e618894397
Marc Fogleman <marcfogleman@gmail.com>
2020-12-12 17:34:02 -0500

    Less embarassing version of day 11_01

--- day11/day11_01.fsx
+++ day11/day11_01.fsx
@@ -2,120 +2,73 @@ open System
 open System.IO
 
 type Space = Empty | Occupied | Floor
+type Layout = Space [,]
 
-let ts1 = File.ReadAllLines "day11_input.txt"
+let map2DWithState fn arr = Array2D.mapi (fn arr) arr
 
-let buildSeats seat =
-  match seat with
-    | 'L' -> Empty
-    | '.' -> Floor
-    | '#' -> Occupied
-    | _ -> failwith "unexpected character"
+let buildSpace c =
+  match c with
+  | '.' -> Floor
+  | '#' -> Occupied
+  | 'L' -> Empty
+  | _ -> failwith "Unexpected character"
+let printChar space =
+  match space with
+  | Floor -> '.'
+  | Occupied -> '#'
+  | Empty -> 'L'
 
-let buildRows row  =
-  Seq.map buildSeats row |> Seq.toList
+let getFrom (raw: char [] []) x y = buildSpace raw.[x].[y]
 
-type Location = Top | Bottom | Left | Right | TopRight | TopLeft | BottomRight | BottomLeft | NotEdge
+let getAdjacentCoords x y =
+  [
+   (x-1, y-1); (x-1, y); (x-1, y+1);
+   (x,   y-1);           (x,   y+1);
+   (x+1, y-1); (x+1, y); (x+1, y+1);
+  ]
 
-let determineLocation (chart: Space list list) (rowNum: int) (colNum: int) =
-  let bottom = (List.length chart) - 1
-  let right = (List.length chart.[0]) - 1
+let getInBoundsSpace (mapState: Layout) (x, y) =
+  try
+    Some (Array2D.get mapState x y)
+  with :? IndexOutOfRangeException ->
+    None
 
-  match (rowNum, colNum) with
-  | (0, 0) -> TopLeft
-  | (0, r) when r = right -> TopRight
-  | (b, 0) when b = bottom -> BottomLeft
-  | (b, r) when (b = bottom && r = right) -> BottomRight
-  | (0, _) -> Top
-  | (b, _) when (b = bottom) -> Bottom
-  | (_, 0) -> Left
-  | (_, r) when (r = right) -> Right
-  | _ -> NotEdge
+let isOccupied s =
+  match s with
+  | Some Occupied -> 1
+  | _ -> 0
 
-let buildAdjacent (chart: Space list list) row col location =
-  let bottom = (List.length chart) - 1
-  let right = (List.length chart.[0]) - 1
-  match location with
-  | TopLeft -> [                chart.[0].[1];
-                  chart.[1].[0];chart.[1].[1];]
-
-  | TopRight -> [chart.[0].[right-1];
-                 chart.[1].[right-1];chart.[1].[right];]
-
-  | BottomLeft -> [chart.[bottom-1].[0]; chart.[bottom-1].[1];
-                                         chart.[bottom].[1]]
-  | BottomRight -> [chart.[bottom-1].[right-1];chart.[bottom-1].[right];
-                    chart.[bottom].[right-1];]
-  | Top -> [
-      chart.[0].[col-1];                chart.[0].[col+1];
-      chart.[1].[col-1];chart.[1].[col];chart.[1].[col+1];
-    ]
-  | Bottom -> [ chart.[bottom-1].[col-1]; chart.[bottom-1].[col]; chart.[bottom-1].[col+1];
-                chart.[bottom].[col-1];                           chart.[bottom].[col+1];]
-  | Left -> [chart.[row-1].[0]; chart.[row-1].[1];
-                                chart.[row].[1];
-            chart.[row+1].[0]; chart.[row+1].[1]]
-  | Right -> [chart.[row-1].[right-1]; chart.[row-1].[right];
-              chart.[row].[right-1];
-              chart.[row+1].[right-1]; chart.[row+1].[right]]
-  | NotEdge -> [chart.[row-1].[col-1]; chart.[row-1].[col]; chart.[row-1].[col+1];
-              chart.[row].[col-1];                          chart.[row].[col+1];
-                chart.[row+1].[col-1]; chart.[row+1].[col]; chart.[row+1].[col+1]]
+let countAdjacentOccupied (mapState: Layout) x y  =
+  getAdjacentCoords x y
+  |> List.map (getInBoundsSpace mapState)
+  |> List.sumBy isOccupied
 
 
-let appRulesToSeats chart rowNum colNum seat =
-  let location = determineLocation chart rowNum colNum
-  let adjacentSpaces = buildAdjacent chart rowNum colNum location
-  let current = chart.[rowNum].[colNum]
-  if current = Empty && not (List.contains(Occupied) adjacentSpaces) then
-    Occupied
-  else if current = Occupied && ( List.filter(fun x -> x = Occupied ) adjacentSpaces |> List.length |> (fun n -> n >= 4) ) then
-    Empty
-  else current
+let parseSpace (mapState: Layout) (x: int) (y: int) (space: Space) =
+  match space with
+  | Empty -> if (countAdjacentOccupied mapState x y) = 0
+             then Occupied
+             else Empty
 
+  | Floor -> Floor
+  | Occupied -> if (countAdjacentOccupied mapState x y) >= 4
+                then Empty
+                else Occupied
 
-let appRulesToRow chart rowNum row =
-  List.mapi (appRulesToSeats chart rowNum) row
+let rec parseUntilStable layout =
+  let newLayout = map2DWithState parseSpace layout
+  if newLayout = layout
+    then newLayout
+    else parseUntilStable newLayout
 
-let eq a1 a2 = a1 = a2
+let countOccupied layout =
+  let mutable total = 0
+  Array2D.iter(fun s -> if s = Occupied then total <- total+1) layout
+  total
 
-let testInner r1 r2 = List.forall2 eq r1 r2
-
-
-let printSeat seat =
-   match seat with
-   | Empty -> 'L'
-   | Floor -> '.'
-   | Occupied -> '#'
-
-let printRow row =
-  List.map printSeat row
-  |> fun x -> String.Join("", x)
-
-let prettyPrint chart =
-  List.map printRow chart
-  |> fun x -> String.Join("\n", x)
-
-let rec recurseTilSame chart =
-  let newChart = List.mapi (appRulesToRow chart) chart
-  let prettyOld = prettyPrint chart
-  let prettyNew = prettyPrint newChart
-
-  if prettyOld = prettyNew then
-    printfn "done"
-    newChart
-    else
-    printfn "testing next"
-    recurseTilSame newChart
-
-
-
-ts1
-  |> Array.map buildRows |> Array.toList
-  |> (fun chart -> List.mapi (appRulesToRow chart) chart)
-  |> recurseTilSame
-  |> prettyPrint
-  |> String.filter (fun c -> c = '#')
-  |> String.length
-  |> Console.WriteLine
-
+File.ReadAllLines "day11_input.txt"
+|> Array.map (fun str -> str.ToCharArray())
+|> (fun raw -> (Array2D.init (Array.length raw) (Array.length raw.[0]) (getFrom raw)): Layout )
+|> parseUntilStable
+|> countOccupied
+|> printfn "%A"
